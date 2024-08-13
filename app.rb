a9b183b535136118f11c9bbdeba02fcc1f9faf25
Anna Villarreal <154029176+AnnaVi11arrea1@users.noreply.github.com>
2024-08-13 17:00:52 +0000

    square working

--- app.rb
+++ app.rb
@@ -2,8 +2,29 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:home, { :layout => :layout })
+end
+
+get("/square/new") do
+  erb(:square, { :layout => :layout })
+end
+
+get("/square/results") do
+  @the_num = params.fetch("user_number").to_f
+
+  @the_result = @the_num ** 2
+
+  erb(:square_results, { :layout => :layout })
+end
+
+get("/square_root/new") do
+  erb(:square_root, { :layout => :layout })
+end
+
+get("/random/new") do
+  erb(:random, { :layout => :layout })
+end
+
+get("/payment/new") do
+  erb(:payment, { :layout => :layout })
 end
