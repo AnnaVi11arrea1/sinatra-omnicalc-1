87a4a2a255eb5e956f4e6a413aa3b36b44d5110d
Anna Villarreal <154029176+AnnaVi11arrea1@users.noreply.github.com>
2024-08-13 19:38:53 +0000

    Changes to be committed: 	modified:   app.rb 	modified:   views/payment.erb 	new file:   views/payment_result.erb 	modified:   views/random.erb	new file:   views/random_result.erb modified:   views/square_root.erb 	new file:   views/square_root_results.erb

--- app.rb
+++ app.rb
@@ -8,23 +8,50 @@ end
 get("/square/new") do
   erb(:square, { :layout => :layout })
 end
-
 get("/square/results") do
   @the_num = params.fetch("user_number").to_f
-
   @the_result = @the_num ** 2
-
   erb(:square_results, { :layout => :layout })
 end
+
+
 
 get("/square_root/new") do
   erb(:square_root, { :layout => :layout })
 end
+get("/square_root/results") do
+  @the_num2 = params.fetch("user_number2").to_f
+  @the_result2 = @the_num2 ** 0.5
+  erb(:square_root_results, { :layout => :layout })
+end
+
+
 
 get("/random/new") do
   erb(:random, { :layout => :layout })
 end
+post("random/result") do
+  min = params.fetch("user_min").to_f
+  max = params.fetch("user_max").to_f
+end
+get("/random/result") do
+  @random_num = rand(min..max)
+  erb(:random_result, { :layout => :layout })
+end
 
 get("/payment/new") do
   erb(:payment, { :layout => :layout })
+end
+get("/payment/result") do
+  rate_pp = params.fetch("r").to_f
+  rate = ((rate_pp/100)/12).to_f
+  r = round(rate).to_fs(:percentage, { :precision => 4 })
+  num_periods = params.fetch("n").to_i
+  n = num_periods * 12
+  pv = params.fetch("pv").to_f
+  numr = (r*pv).to_f
+  denom = (1-(1 + r)**-n).to_f
+  @payment = (numer/denom).to_fs(:currency)
+  
+  erb(:payment_result, { :layout => :layout })
 end
